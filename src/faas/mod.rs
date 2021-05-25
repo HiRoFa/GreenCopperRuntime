44082a7723b5c15a330313e9d46c02bc77f5e399
Andries Hiemstra <andries@fam-hiemstra.nl>
2021-05-25 13:45:56 +0200

    wip

--- src/faas/mod.rs
+++ src/faas/mod.rs
@@ -8,6 +8,7 @@ pub struct GreenCopperFaas {}
 
 // todo, can we share modules between contexts? or do we need to have one context with all modules..
 // modules are not GCed in quickjs, we'll to actively kill runtimes every now and then if so
+// todo create a feature for this so it can be called from JS (should even be able to start a faas service for StarLight from quickjs and such
 
 impl GreenCopperFaas {
     pub fn new(
@@ -22,9 +23,10 @@ impl GreenCopperFaas {
     ///
     pub fn run(
         &self,
-        _function_id: &str,
-        _function_specs: Vec<String>,
-        _args: Vec<EsValueFacade>,
+        _function_id: &str, // this is the main id of the faas function, e.g. handleRequest
+        _function_specs: Vec<String>, // this can be used to specify conditions for determining which faas function to use, e.g. vec!["path=/home", "port=8080", "domain=localhost"]
+        _args: Vec<EsValueFacade>,    // arguments to pass to the function
+        _runtime_specs: (), // specify how the runtime should be built, e.g. quickjs/boa/starlight/spidermonkey / other builder opts
     ) -> Option<Box<dyn Future<Output = Result<EsValueFacade, JsError>>>> {
         unimplemented!()
     }
@@ -35,6 +37,7 @@ pub fn run(
     _function_id: &str,
     _function_specs: Vec<String>,
     _args: Vec<EsValueFacade>,
+    _runtime_specs: (),
 ) -> Option<Box<dyn Future<Output = Result<EsValueFacade, JsError>>>> {
     unimplemented!()
 }
