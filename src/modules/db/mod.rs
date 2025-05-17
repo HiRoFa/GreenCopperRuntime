291125ad096af9553cd690d63f2dc4ce8069809a
andries@fam-hiemstra.nl <andries@fam-hiemstra.nl>
2025-05-17 10:56:06 +0200

    removed mysql in favor of sqlx

--- src/modules/db/mod.rs
+++ src/modules/db/mod.rs
@@ -1,15 +1,10 @@
 use quickjs_runtime::builder::QuickJsRuntimeBuilder;
 
-#[cfg(any(feature = "all", feature = "db", feature = "mysql"))]
-pub mod mysql;
-
-#[cfg(any(feature = "all", feature = "db", feature = "sqlx"))]
+#[cfg(feature = "sqlx")]
 pub mod sqlx;
 
 pub(crate) fn init(builder: QuickJsRuntimeBuilder) -> QuickJsRuntimeBuilder {
-    #[cfg(any(feature = "all", feature = "db", feature = "mysql"))]
-    let builder = mysql::init(builder);
-    #[cfg(any(feature = "all", feature = "db", feature = "sqlx"))]
+    #[cfg(feature = "sqlx")]
     let builder = sqlx::init(builder);
     builder
 }
